6c2b2eecbe582088f46a05c56d1cf6b7b63aa69d
Wahome <vinwahome@gmail.com>
2024-04-17 17:18:18 +0300

    Implement 'download cv' feature

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,10 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useRef } from 'react'
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from "framer-motion"
+import { Link } from 'react-scroll';
+import Image from 'next/image'
+import avi from '../../../public/images/profile-pic.jpg'
+
 
 
 function HeroSection() {
+    const contactRef = useRef(null);
+
+    const handleDownload = () => {
+        // Replace 'your_cv.pdf' with the actual path to your CV file
+        const downloadUrl = '/assests/resume.pdf';
+        window.open(downloadUrl, '_blank');
+      };
+
   return (
     <section>  
         <motion.div 
@@ -36,14 +48,22 @@ function HeroSection() {
                 />
                 </h1>
                 <p className='text-[#888f96] my-4 text-center sm:text-left'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry text of the printing and typesetting industry
+                Welcome to my portfolio website! As a passionate web developer, I specialize in crafting immersive digital experiences that seamlessly blend creativity and functionality. With a keen eye for detail and a dedication to staying at the forefront of technology, I strive to bring my clients' visions to life through elegant, user-centric design and robust, scalable code.
                 </p>
 
                 <div className='flex flex-col w-2/3 md:w-full mx-auto md:flex-row md:gap-8'>
-                    <button className='bg-gradient-to-br from-blue-100 via-blue-500 to-blue-700 hover:bg-[#0B0033] text-white  hover:text-white hover:border rounded-full  sm:w-fit px-6 py-3 my-4 font-bold '>
-                        Hire Me
-                    </button>
-                    <button className='bg-gradient-to-br from-blue-100 via-blue-500 to-blue-700 hover:bg-[#e9e9e9] text-white hover:text-[#545061] border rounded-full  my-4 sm:w-fit text-center px-1 py-1 font-bold'>
+                   
+                    <Link
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        offset={-70} // adjust this value according to your layout
+                        className='bg-gradient-to-br from-blue-100 via-blue-500 to-blue-700 hover:bg-[#0B0033] text-white  hover:text-white hover:border rounded-full  sm:w-fit px-6 py-3 md:px-auto md:py-4 my-4 font-bold text-center cursor-pointer'                
+                        >
+                        Hire me
+                    </Link>
+                    <button onClick={handleDownload} className='bg-gradient-to-br from-blue-100 via-blue-500 to-blue-700 hover:bg-[#e9e9e9] text-white hover:text-[#545061] border rounded-full  my-4 sm:w-fit text-center px-1 py-1 font-bold'>
                         <span className='block bg-[#0B0033] rounded-full px-5 py-3'>
                             Download CV
                         </span>
@@ -52,7 +72,7 @@ function HeroSection() {
             </div>
 
             <div className=' mx-auto rounded-full w-60 h-60 md:w-80 md:h-80 bg-[#110344]'>
-                <img src='https://images.unsplash.com/photo-1570215171424-f74325192b55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHNvZndhcmUlMjBkZXZlbG9wZXJ8ZW58MHx8MHx8fDA%3D' alt='profile-pic' className='rounded-full  w-60 h-60 md:w-80 md:h-80 p-2'/>
+                <Image src={avi} alt='profile-pic' className='rounded-full  w-60 h-60 md:w-80 md:h-80 p-2 object-cover'/>
             </div>
         </motion.div>
     </section>
